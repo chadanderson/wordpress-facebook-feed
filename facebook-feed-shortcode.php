99ccb4733e84d01d747da6201d71c1db6e001df8
Chad Anderson <chad@Chad-Andersons-iMac.local>
2013-05-21 04:34:02 -0400

    Shortcode Launch

---
+++ facebook-feed-shortcode.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+Add Facebook feed shortcode to WordPress
+*/
+function get_facebook_updates( $atts, $content = null ) {
+
+	extract( shortcode_atts( array( 
+		'facebook_access_token' => '',
+		'facebook_page_name' => ''
+		), $atts ) );
+
+	// Extract the page ID from the Facebook Page
+	$facebook_page_data = file_get_contents( 'https://graph.facebook.com/' . $facebook_page_name );
+	$facebook_page_data = json_decode( $facebook_page_data );
+	$facebook_page_id = $facebook_page_data->id;
+
+	// Get the contents of the Facebook page
+	$facebook_page_content = file_get_contents( 'https://graph.facebook.com/' . $facebook_page_id . '/feed?access_token=' . $facebook_access_token );
+
+	// Interpret data with JSON
+	$facebook_data = json_decode($facebook_page_content);
+	$html = '<h2 class="facebook-feed-title">' . __( 'Facebook Feed', 'facebook-feed-shortcode' ) . '</h2>';
+	$html .= '<ul class="facebook-feed">';
+
+	// Loop through data for each feed item
+	$i = 1;
+	foreach ($facebook_data->data as $news ) {
+		if($i < 4) { 
+			$StatusID = explode("_", $news->id);?>
+			<?php if (!empty($news->message))
+			{ 
+				$content = substr($news->message, 0, 160); 
+				$html .= '<li>'.$content . '...<br />';
+				$html .= '<a href="http://www.facebook.com/' . $facebook_page_name . '">View the full entry</a></li>';
+			}
+		}
+		$i++;
+	}
+	$html .= '</ul>';
+	$html .= '<p><button class="facebook-feed-button"><a href="http://www.facebook.com/' . $facebook_page_name . '">Connect on Facebook</a></button></p>';
+	return $html;
+}
+
+add_shortcode('facebook_feed', 'get_facebook_updates');
+
+?> 
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Facebook Feed Shortcode for WordPress ===
+
+Author: Chad Anderson
+
+
+Allows shortcode to be used to insert Facebook feed.
+
+== Description ==
+This creates a shortcode that will allow you to insert a Facebook feed anywhere on your WordPress site.
+
+
+
+== Installation ==
+1. Copy and paste the script to your functions.php file in your theme directory.
+2. Add [facebook_feed] shortcode to any page or post in WordPress.
+3. Requires the following attributes: facebook_access_token , facebook_page_name
+
+Example:
+
+[facebook_feed facebook_access_token="YOUR-ACCESS-TOKEN" facebook_page_name="YOUR-PAGE-NAME"]
+
+
+
+== Stylings ==
+To style your Facebook Feed, add the following CSS to your stylesheet\:
+
+.facebook-feed-title {
+
+}
+
+.facebook-feed {
+
+}
+
+.facebook-feed li {
+
+}
+
+.facebook-feed-button {
+
+}
